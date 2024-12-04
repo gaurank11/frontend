2e9744f7cb09e52586282b5c543e2593c66e2015
gaurank11 <sharmagaurank63@gmail.com>
2024-12-04 17:38:20 +0530

    first commit

---
+++ src/pages/Home2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Services from "../components/Services";
+
+const Home2 = () => {
+  return (
+    <div>
+     
+      <Services/>
+
+    </div>
+  );
+};
+
+export default Home2;
